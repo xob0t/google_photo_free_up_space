57663a82cb55f176c1e716d37a97d91ceab0820b
xob0t <32616886+xob0t@users.noreply.github.com>
2023-12-29 04:13:34 +0300

    page token backup

--- photos_db_update.py
+++ photos_db_update.py
@@ -24,6 +24,13 @@ def create_table_if_not_exists(db_filename):
         )
     ''')
 
+    # Create the 'script_data' table if not exists
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS script_data (
+            last_processed_page TEXT PRIMARY KEY
+        )
+    ''')
+
     sqlite_connection.commit()
     cursor.close()
 
@@ -77,26 +84,59 @@ def new_service():
     photos_api = build('photoslibrary', 'v1', credentials=creds, static_discovery=False)
     return photos_api
 
-
 def get_photos_page(photos_api, next_page_token):
-    items_request = photos_api.mediaItems().list(pageSize=100, pageToken=next_page_token)
+    pageSize = 100 # 100 is Max
+    items_request = photos_api.mediaItems().list(pageSize=pageSize, pageToken=next_page_token)
     response = items_request.execute()
     items = response.get('mediaItems', [])
     next_page_token = response.get('nextPageToken') or False
     return items, next_page_token
 
+def update_page_token(db_filename, next_page_token):
+    sqlite_connection = sqlite3.connect(db_filename)
+    cursor = sqlite_connection.cursor()
+    
+    # Check if the first row exists
+    cursor.execute("SELECT ROWID FROM script_data WHERE ROWID = 1")
+    row = cursor.fetchone()
+    
+    if row:
+        # Update the existing row
+        cursor.execute("UPDATE script_data SET last_processed_page = ? WHERE ROWID = 1", (next_page_token,))
+    else:
+        # Insert a new row
+        cursor.execute("INSERT INTO script_data (ROWID, last_processed_page) VALUES (1, ?)", (next_page_token,))
+    
+    cursor.close()
+    sqlite_connection.commit()
+
+
+def get_page_token(db_filename):
+    sqlite_connection = sqlite3.connect(db_filename)
+    cursor = sqlite_connection.cursor()
+    cursor.execute("SELECT last_processed_page FROM script_data WHERE ROWID = 1")
+    row = cursor.fetchone()
+    cursor.close()
+    sqlite_connection.close()
+
+    if row:
+        return row[0] if row[0] != '0' else None
+    else:
+        return None
 
 def main():
     photos_api = new_service()
     db_filename = "photos_db.sqlite"
     create_table_if_not_exists(db_filename)
-    next_page_token = None
+    next_page_token = get_page_token(db_filename)
     while True:
+        update_page_token(db_filename, next_page_token)
         items, next_page_token = get_photos_page(photos_api, next_page_token)
         for item in items:
             print(item['filename'])
             insert_or_update_media(db_filename, item)
         if not next_page_token:
+            update_page_token(db_filename, next_page_token)
             break
 
 
