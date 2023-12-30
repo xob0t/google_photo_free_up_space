cc510b0d55ec28385f5d9b583a469a37d8690290
xob0t <32616886+xob0t@users.noreply.github.com>
2023-12-30 19:24:35 +0300

    pep

--- chrome_google_login.py
+++ chrome_google_login.py
@@ -4,7 +4,7 @@ import undetected_chromedriver as uc
 
 driver_data_path = os.path.join(os.getcwd(), "driver_data")
 
-driver = uc.Chrome(headless=False, user_data_dir = driver_data_path)
+driver = uc.Chrome(headless=False, user_data_dir=driver_data_path)
 
 input("Please log into Google. Google Photos media info panel must be opened. Press Enter to continue")
 driver.quit()
